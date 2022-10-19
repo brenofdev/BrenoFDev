5c56519068ed778b6589a16ba991af12f93e8cf7
Breno <brenofcb1@gmail.com>
2022-10-18 23:48:16 -0300

    Initializing project

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from '../styles/pages/home.module.css';
+
+export default function Home() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+
